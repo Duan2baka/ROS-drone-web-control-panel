9b609545432bbe2233d961280c3679e68f8ce993
Duan2baka <duan2baka@gmail.com>
2023-05-25 12:00:25 +0800

    draw the arrow on the canvas

--- js/map.js
+++ js/map.js
@@ -21,8 +21,14 @@ class MapComponent{
         this.map_width = 0;
         this.map_height = 0;
         this.set_navigation = false;
+        this.add_goal = false;
         this.canvas_stamp = -1;
-        this.time_gap = 0.01;
+        this.time_gap = 0;
+        this.path_point = [];
+        
+        class PathPoint{
+
+        };
         // this.update_time = attrs.update_time || 0.2;
         // this.time_stamp = -1;
         // this.time_stamp_ns = -1;
@@ -56,47 +62,59 @@ class MapComponent{
             //self.startX = e.clientX - self.canvas.offsetLeft - self.div.offsetLeft + window.scrollX;
             self.startX = e.offsetX;
             self.startY = e.offsetY;
+            self.start_x_map = self.getMapPosition(e.offsetX, e.offsetY, self.map_width, self.map_height, self)[0];
+            self.start_y_map = self.getMapPosition(e.offsetX, e.offsetY, self.map_width, self.map_height, self)[1];
             self.start_anchor_x = self.anchor_x;
             self.start_anchor_y = self.anchor_y;
         });
 
         this.canvas.addEventListener('mousemove', function(e) {
             if (self.mouseDown) {
-                var mouseX = e.offsetX;
-                var mouseY = e.offsetY;
+                let mouseX = e.offsetX;
+                let mouseY = e.offsetY;
                 if(self.set_navigation){
-
-
+                    self.updateMap(-1);
+                    self.drawArrow(self.start_x_map, self.start_y_map,
+                        self.getMapPosition(e.offsetX, e.offsetY, self.map_width, self.map_height, self)[0],
+                        self.getMapPosition(e.offsetX, e.offsetY, self.map_width, self.map_height, self)[1]);
                 }
                 else{
-                    console.log(213);
-                    self.anchor_x = self.start_anchor_x + (mouseX - self.startX);
-                    self.anchor_y = self.start_anchor_y + (mouseY - self.startY);
+                    self.anchor_x = self.start_anchor_x + (mouseX - self.startX) / self.scale;
+                    self.anchor_y = self.start_anchor_y + (mouseY - self.startY) / self.scale;
                     self.updateMap(-1);
                 }
+                e.stopPropagation();
             }
         });
 
         this.canvas.addEventListener('mouseup', function(e) {
             self.mouseDown = false;
-            var mouseX = e.offsetX;
-            var mouseY = e.offsetY;
+            console.log(e.offsetX, e.offsetY);
+            let mouseX = self.getMapPosition(e.offsetX, e.offsetY, self.map_width, self.map_height, self)[0];
+            let mouseY = self.getMapPosition(e.offsetX, e.offsetY, self.map_width, self.map_height, self)[1];
             if(self.set_navigation){
                 if(map_resolution == -1) return;
-                var real_x = self.startX * map_resolution + map_origin_x; 
-                var real_y = self.startY * map_resolution + map_origin_y;
+                let real_x = self.start_x_map * map_resolution + map_origin_x; 
+                let real_y = self.start_y_map * map_resolution + map_origin_y;
     
-                var orientation = Math.atan(-(mouseY - self.startY) / (mouseX - self.startX)) / Math.PI;
-                if(mouseX < self.startX && mouseY <= self.startY) orientation += 1;
-                else if(mouseX < self.startX && mouseY >= self.startY) orientation -= 1; 
+                let orientation = Math.atan(-(mouseY - self.start_y_map) / (mouseX - self.start_x_map)) / Math.PI;
+                if(mouseX < self.start_x_map && mouseY <= self.start_y_map) orientation += 1;
+                else if(mouseX < self.start_x_map && mouseY >= self.start_y_map) orientation -= 1; 
                 orientation *= -1;
                 // console.log(orientation);
-                 console.log(self.startX, self.startY);
-                // set_goal(real_x, real_y, orientation);
+                //console.log(self.startX, self.startY);
+                self.updateMap(-1);
+                self.set_navigation = false;
+                console.log(real_x, real_y);
+                if(self.add_goal){
+
+                }
+                ///else set_goal(real_x, real_y, orientation);
+                ///set_goal(real_x, real_y, orientation);
             }
         });
         this.canvas.addEventListener('mousewheel', function(e){
-            var previous_scale = self.scale;
+            //let previous_scale = self.scale;
             if(e.wheelDelta > 0){
                 self.scale *= 1.1;
                 if(self.scale < 7){
@@ -107,6 +125,7 @@ class MapComponent{
             if(e.wheelDelta < 0)
                 self.scale /= 1.1;
             self.updateMap(-1);
+            e.stopPropagation();
         });
     }
     
@@ -123,11 +142,11 @@ class MapComponent{
     }
     
     getMapPosition(x, y, width, height, self){ // canvas to map
-        var relative_x = x - width / 2;
-        var relative_y = y - height / 2;
+        let relative_x = x - width / 2;
+        let relative_y = y - height / 2;
 
-        var target_x = parseInt(relative_x / self.scale + width / 2 - self.anchor_x);
-        var target_y = parseInt(relative_y / self.scale + height / 2 - self.anchor_y);
+        let target_x = parseInt(relative_x / self.scale + width / 2 - self.anchor_x);
+        let target_y = parseInt(relative_y / self.scale + height / 2 - self.anchor_y);
 
         return [target_x, target_y];
     }
@@ -145,23 +164,23 @@ class MapComponent{
             if(this.message == -1) return;
             else mapMessage = this.message;
         else this.message = mapMessage;
-        var width = mapMessage.info.width, height = mapMessage.info.height;
+        let width = mapMessage.info.width, height = mapMessage.info.height;
         this.map_width = width;
         this.map_height = height;
-        var imageData = new ImageData(width, height);
+        let imageData = new ImageData(width, height);
         function getIndex(x, y, width){
             return x + y * width;
         }
         function getPosition(index, width){
             return [index % width, parseInt(index / width)];
         }
-        for (var i = 0; i < width * height; i++) {
-            var tmp_x, tmp_y;
+        for (let i = 0; i < width * height; i++) {
+            let tmp_x, tmp_y;
             tmp_x = getPosition(i, width)[0];
             tmp_y = getPosition(i, width)[1];
 
-            var target_x = this.getMapPosition(tmp_x, tmp_y, width, height, this)[0];
-            var target_y = this.getMapPosition(tmp_x, tmp_y, width, height, this)[1];
+            let target_x = this.getMapPosition(tmp_x, tmp_y, width, height, this)[0];
+            let target_y = this.getMapPosition(tmp_x, tmp_y, width, height, this)[1];
 
             let index = getIndex(target_x, target_y, width);
 
@@ -197,11 +216,11 @@ class MapComponent{
         if(this.canvas.hidden) return;
         this.canvas.width = width;
         this.canvas.height = height;
-        var context = this.canvas.getContext("2d");
+        let context = this.canvas.getContext("2d");
         context.putImageData(imageData, 0, 0);
         if(this.pos_x != -1){
-            var target_x = this.getCanvasPosition(this.pos_x, this.pos_y, width, height, this)[0];
-            var target_y = this.getCanvasPosition(this.pos_x, this.pos_y, width, height, this)[1];
+            let target_x = this.getCanvasPosition(this.pos_x, this.pos_y, width, height, this)[0];
+            let target_y = this.getCanvasPosition(this.pos_x, this.pos_y, width, height, this)[1];
             if(!(target_x >= width || target_x < 0 || target_y >= height || target_y < 0)){
                 context.fillStyle = 'green';
                 context.beginPath();
@@ -209,6 +228,30 @@ class MapComponent{
                 context.fill();
             }
         }
+    }
+
+    drawArrow(start_x_map, start_y_map, end_x_map, end_y_map){
+        let start_x = this.getCanvasPosition(start_x_map, start_y_map, this.map_width, this.map_height, this)[0];
+        let start_y = this.getCanvasPosition(start_x_map, start_y_map, this.map_width, this.map_height, this)[1];
+        let end_x = this.getCanvasPosition(end_x_map, end_y_map, this.map_width, this.map_height, this)[0];
+        let end_y = this.getCanvasPosition(end_x_map, end_y_map, this.map_width, this.map_height, this)[1];
+        //console.log(start_x, start_y, end_x, end_y);
+        let context = this.canvas.getContext("2d");
+        let angle = Math.atan2(end_y - start_y, end_x - start_x);
+        let arrowLength = 10;
+        context.strokeStyle = context.fillStyle = 'black';
+      
+        context.beginPath();
+        context.moveTo(start_x, start_y);
+        context.lineTo(end_x, end_y);
+        context.stroke();
+      
+        context.beginPath();
+        context.moveTo(end_x, end_y);
+        context.lineTo(end_x - arrowLength * Math.cos(angle - Math.PI / 6), end_y - arrowLength * Math.sin(angle - Math.PI / 6));
+        context.lineTo(end_x - arrowLength * Math.cos(angle + Math.PI / 6), end_y - arrowLength * Math.sin(angle + Math.PI / 6));
+        context.lineTo(end_x, end_y);
+        context.fill();
     }
     //<div id="lidarDiv" style="text-align: center;"><canvas id="lidarCanvas"></canvas></div>
 }
